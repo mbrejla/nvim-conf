0f2e62a729907d3ae4707e2fb9eabe9b7e5ca30e
Markus Brejla <m.brejla@gfx3d.net>
2024-06-03 23:18:23 +0200

    fixed identation

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,12 +1,12 @@
 return {
-	"akinsho/toggleterm.nvim",
-	config = function()
-		local tterm = require("toggleterm")
-		tterm.setup({
-			size = 20,
-			open_mapping = [[<C-\>]],
-			start_in_insert = true,
-			direction = "float",
-		})
-	end,
+  "akinsho/toggleterm.nvim",
+  config = function()
+    local tterm = require("toggleterm")
+    tterm.setup({
+      size = 20,
+      open_mapping = [[<C-\>]],
+      start_in_insert = true,
+      direction = "float"
+    })
+  end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,18 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "python" },
-			auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {"lua", "vim", "vimdoc", "javascript", "html", "python"},
+      auto_install = true,
+      sync_install = false,
+      highlight = {
+        enable = true
+      },
+      indent = {
+        enable = true
+      }
+    })
+  end
 }
